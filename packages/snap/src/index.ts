bb8ff43070c49bd84c81fe1a6c7d9e742ccd2b2b
kevin-222 <110803206+kevin-222@users.noreply.github.com>
2024-01-16 14:37:48 +0800

    feat: name lookup

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -3,34 +3,24 @@ import type { OnNameLookupHandler } from '@metamask/snaps-sdk';
 import { getWeb3Name } from './utils';
 
 export const onNameLookup: OnNameLookupHandler = async ({
-  chainId,
+  // chainId,
   domain,
-  address,
+  // address,
 }) => {
   const web3Name = getWeb3Name();
-  console.log('onNameLookup22', { chainId, domain, address });
   try {
     if (domain) {
       const tld = domain.split('.').pop();
       if (!tld || tld.length < 3) {
         return null;
       }
-      // eslint-disable-next-line no-restricted-globals
-      console.log(window.ethereum);
-      const res = await web3Name.getAddress('spaceid.bnb', {
-        rpcUrl: 'https://arb1.arbitrum.io/rpc',
-      });
-      console.log('onNameLookup res11', domain, res);
+      const res = await web3Name.getAddress(domain);
       if (res) {
         return { resolvedAddress: res };
       }
     }
-    // if (address) {
-    //   return null;
-    // }
     return null;
   } catch (error: any) {
-    console.log('onNameLookup error', error);
     return null;
   }
 };
