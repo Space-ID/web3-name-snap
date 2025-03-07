29a4e4dc33afb41649bb87f7b591d58240e560cd
stanli1231 <stan.li@space.id>
2025-03-07 11:39:29 +0800

    feat: add fix

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -3,16 +3,22 @@ import type { OnNameLookupHandler } from '@metamask/snaps-sdk';
 import { getWeb3Name } from './utils';
 
 export const onNameLookup: OnNameLookupHandler = async ({ domain }) => {
-  if (!domain) return null;
+  if (!domain) {
+    return null;
+  }
   const web3Name = getWeb3Name(domain);
-  console.log('domain', domain);
+  console.log('domain', domain, web3Name);
   try {
     if (domain) {
       const tld = domain.split('.').pop();
       if (!tld) {
+        console.log('!tld', tld);
         return null;
       }
-      const res = domain.includes('@') ? await web3Name.getAddress({ name: domain, chainId: 1 }) : await web3Name.getAddress(domain)
+      console.log('tld', tld);
+      const res = domain.includes('@')
+        ? await web3Name.getAddress({ name: domain, chainId: 1 })
+        : await web3Name.getAddress(domain);
       console.log('resresres', res);
       if (res) {
         return {
--- packages/snap/src/utils/index.ts
+++ packages/snap/src/utils/index.ts
@@ -1,22 +1,19 @@
 import { createWeb3Name, createPaymentIdName } from '@web3-name-sdk/core';
 
-
 let web3Name: any;
 let paymentIdName: any;
 
 const getWeb3Name = (domain: string) => {
   if (domain.includes('@')) {
     if (!paymentIdName) {
+      console.log('paymentIdName', paymentIdName);
       paymentIdName = createPaymentIdName();
       return paymentIdName;
     }
-
-  } else {
-    if (!web3Name) {
-      web3Name = createWeb3Name();
-      return web3Name;
-    }
-
+  } else if (!web3Name) {
+    console.log('web3Name', web3Name);
+    web3Name = createWeb3Name();
+    return web3Name;
   }
 };
 
