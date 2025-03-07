5dac3fef01e60c40767490cfde9728dcb0025027
stanli1231 <stan.li@space.id>
2025-03-07 13:12:24 +0800

    Feat/payment id snap (#104)
    * feat: add payment-id sdk
    
    * feat: add fix
    
    * feat: add
    
    * feat: add fix

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,18 +1,31 @@
+
 import type { OnNameLookupHandler } from '@metamask/snaps-sdk';
 
-import { getWeb3Name } from './utils';
+import { getWeb3Name, getWeb3PaymentIdName } from './utils';
 
 export const onNameLookup: OnNameLookupHandler = async ({ domain }) => {
+  // const web3Name = getWeb3Name();
   if (!domain) return null;
-  const web3Name = getWeb3Name(domain);
-  console.log('domain', domain);
   try {
+    let web3Name;
+    if (domain.includes('@')) {
+      console.log('111111')
+      web3Name = getWeb3PaymentIdName();
+    } else {
+      console.log('222222')
+      web3Name = getWeb3Name();
+    }
+    if (!web3Name) return null;
     if (domain) {
       const tld = domain.split('.').pop();
       if (!tld) {
+        console.log('Nottld', tld);
         return null;
       }
-      const res = domain.includes('@') ? await web3Name.getAddress({ name: domain, chainId: 1 }) : await web3Name.getAddress(domain)
+      console.log('tld', tld);
+      const res = domain.includes('@')
+        ? await web3Name.getAddress({ name: domain, chainId: 1 })
+        : await web3Name.getAddress(domain);
       console.log('resresres', res);
       if (res) {
         return {
@@ -28,3 +41,32 @@ export const onNameLookup: OnNameLookupHandler = async ({ domain }) => {
   }
   return null;
 };
+
+
+// export const onNameLookup: OnNameLookupHandler = async ({ domain }) => {
+//   const web3Name = getWeb3Name();
+//   console.log('domain', domain, web3Name);
+//   try {
+//     if (domain) {
+//       const tld = domain.split('.').pop();
+//       if (!tld) {
+//         console.log('!tld', tld);
+//         return null;
+//       }
+//       console.log('tld', tld);
+//       const res = await web3Name.getAddress()
+//       console.log('resresres', res);
+//       if (res) {
+//         return {
+//           resolvedAddresses: [
+//             { protocol: 'SPACE ID', resolvedAddress: res, domainName: domain },
+//           ],
+//         };
+//       }
+//       return null;
+//     }
+//   } catch (error: any) {
+//     return null;
+//   }
+//   return null;
+// };
--- packages/snap/src/utils/index.ts
+++ packages/snap/src/utils/index.ts
@@ -1,23 +1,20 @@
 import { createWeb3Name, createPaymentIdName } from '@web3-name-sdk/core';
 
-
 let web3Name: any;
 let paymentIdName: any;
 
-const getWeb3Name = (domain: string) => {
-  if (domain.includes('@')) {
-    if (!paymentIdName) {
-      paymentIdName = createPaymentIdName();
-      return paymentIdName;
-    }
-
-  } else {
-    if (!web3Name) {
-      web3Name = createWeb3Name();
-      return web3Name;
-    }
-
+const getWeb3Name = () => {
+  if (!web3Name) {
+    web3Name = createWeb3Name()
   }
+  return web3Name
 };
 
-export { getWeb3Name };
+const getWeb3PaymentIdName = () => {
+  if (!paymentIdName) {
+    paymentIdName = createPaymentIdName()
+  }
+  return paymentIdName
+};
+
+export { getWeb3Name, getWeb3PaymentIdName };
